63e1bf9ffb5e9a86196ed8cb26e01ab67f2fc815
vseplet <vseplet@gmai.com>
2026-04-18 13:50:33 +0300

    paint greeting with a rainbow in TTY mode

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,12 @@
-console.log("hello, brew!");
+const text = "hello, brew!";
+const palette = ["\x1b[91m", "\x1b[93m", "\x1b[92m", "\x1b[96m", "\x1b[94m", "\x1b[95m"];
+const reset = "\x1b[0m";
+
+const useColor = process.stdout.isTTY && !process.env.NO_COLOR;
+
+if (useColor) {
+  const out = [...text].map((ch, i) => palette[i % palette.length] + ch).join("") + reset;
+  console.log(out);
+} else {
+  console.log(text);
+}
